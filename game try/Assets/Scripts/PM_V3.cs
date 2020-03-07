176ed427be2bcb4c81b401e396e3869a1d5248e2
SeefatHimel <54713352+SeefatHimel@users.noreply.github.com>
2020-03-08 02:50:06 +0600

    Bug free
    almost no previous bugs

---
+++ game try/Assets/Scripts/PM_V3.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PM_V3 : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+
+    Vector2 movemoent;
+    Vector2 movemoent2;
+
+    public FloatingJoystick joystick;
+    // Start is called before the first frame update
+   
+
+
+    private void Update()
+    {
+
+        rb.velocity = new Vector2(joystick.Horizontal * 20 * moveSpeed, joystick.Vertical * 15 * moveSpeed);
+
+        movemoent.x = Input.GetAxisRaw("Horizontal");
+        movemoent.y = Input.GetAxisRaw("Vertical");
+
+
+
+        movemoent2.x = joystick.Horizontal;
+        movemoent2.y = joystick.Vertical;
+    }
+
+
+
+    private void FixedUpdate()
+    {
+
+
+        rb.MovePosition(rb.position + movemoent * moveSpeed * Time.fixedDeltaTime);
+
+
+        rb.MovePosition(rb.position + movemoent2 * moveSpeed * Time.fixedDeltaTime);
+
+    }
+
+
+
+}
